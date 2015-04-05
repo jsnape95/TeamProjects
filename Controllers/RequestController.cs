343f25535b560e9b0c6190ce7107e4a8b363aee8
jsnape95 <jsnape95@gmail.com>
2015-04-05 19:07:25 +0100

    Joining in linq statement
    Added join onto the linq statement so that the ViewRequest table now
    shows the modules and the weeks

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -3,23 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamProjects2.Models;
 
-namespace TeamProjects.Controllers
+namespace TeamProjects2.Controllers
 {
     public class RequestController : Controller
     {
-        //
-        // GET: /Request/
-
+        [Authorize]
         public ActionResult MakeRequest()
         {
             return View();
         }
 
+        [Authorize]
         public ActionResult ViewRequest()
         {
-            return View();
-        }
+            IList<ViewRequest> oRequestID = null;
+            using (team07Entities oteam07ViewRequest = new team07Entities())
+            {
+                oRequestID = (from Request in oteam07ViewRequest.Requests
+                                join Modules in oteam07ViewRequest.Modules
+                                on Request.moduleCode equals Modules.moduleCode
+                                join WeekView in oteam07ViewRequest.Weeks
+                                on Request.weekID equals WeekView.weekID
+                                select new ViewRequest()
+                                {
+                                    requestID = Request.requestID,
+                                    status = Request.status,
+                                    day = Request.day,
+                                    periodStart =Request.periodStart,
+                                    periodEnd = Request.periodEnd,
+                                    semester = Request.semester,
+                                    year =Request.year,
+                                    round = Request.round,
+                                    moduleCode = Modules.moduleCode,
+                                    moduleTitle = Modules.moduleTitle,
+                                    week1 = WeekView.week1,
+                                    week2 = WeekView.week2,
+                                    week3 = WeekView.week3,
+                                    week4 = WeekView.week4,
+                                    week5 = WeekView.week5,
+                                    week6 = WeekView.week6,
+                                    week7 = WeekView.week7,
+                                    week8 = WeekView.week8,
+                                    week9 = WeekView.week9,
+                                    week10 = WeekView.week10,
+                                    week11 = WeekView.week11,
+                                    week12 = WeekView.week12,
+                                    week13 = WeekView.week13,
+                                    week14 = WeekView.week14,
+                                    week15 = WeekView.week15
 
+                                }).ToList(); //putting the table User into variable called oUser ---- query called linq 
+            }
+            return View(oRequestID);
+        }
     }
 }
--- Models/ViewRequest.cs
+++ Models/ViewRequest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace TeamProjects.Models
+namespace TeamProjects2.Models
 {
     public class ViewRequest
     {
@@ -15,6 +15,24 @@ namespace TeamProjects.Models
         public int semester { get; set; }
         public int year { get; set; }
         public int round { get; set; }
+        public string moduleCode { get; set; }
+        public string moduleTitle { get; set; }
+        public int week1 { get; set; }
+        public int week2 { get; set; }
+        public int week3 { get; set; }
+        public int week4 { get; set; }
+        public int week5 { get; set; }
+        public int week6 { get; set; }
+        public int week7 { get; set; }
+        public int week8 { get; set; }
+        public int week9 { get; set; }
+        public int week10 { get; set; }
+        public int week11 { get; set; }
+        public int week12 { get; set; }
+        public int week13 { get; set; }
+        public int week14 { get; set; }
+        public int week15 { get; set; }
+
 
     }
 }
